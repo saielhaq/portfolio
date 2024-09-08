af221d00e3eedc5412cb499790432de2cda2b8bb
saielhaq <saielhaqs@gmail.com>
2024-09-09 00:03:30 +0100

    Updated Intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -64,10 +64,10 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Saad.</span> I'm a{" "}
-        <span className="font-bold">Frontend developer</span> with{" "}
+        <span className="font-bold">Fullstack developer</span> with{" "}
         <span className="font-bold">3 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">Angular and Spring Boot</span>.
       </motion.h1>
 
       <motion.div
